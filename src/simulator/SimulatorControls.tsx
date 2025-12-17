4f21d9ebda8e040f5910df484392a6287be243ae
Mike Cann <mike.cann@gmail.com>
2025-12-17 10:35:44 +0800

    fixing issue with simulator controls

--- src/simulator/SimulatorControls.tsx
+++ src/simulator/SimulatorControls.tsx
@@ -1,19 +1,23 @@
 import { OrbitControls, FlyControls } from "@react-three/drei";
 import { useSimulator } from "./SimulatorContext";
 import { FPSControls } from "./FPSControls";
+import { exhaustiveCheck } from "../../shared/misc";
 
 export function SimulatorControls() {
   const simulator = useSimulator();
   const app = simulator.app;
   const project = app.getProject();
+  const cameraControl = project.settings.cameraControl ?? "orbit";
 
-  if (project.settings.cameraControl === "orbit")
+  if (cameraControl === "orbit")
     return <OrbitControls enableDamping makeDefault />;
 
-  if (project.settings.cameraControl === "fly")
+  if (cameraControl === "fly")
     return (
       <FlyControls makeDefault movementSpeed={10} rollSpeed={0.2} dragToLook />
     );
 
-  if (project.settings.cameraControl === "first_person") return <FPSControls />;
+  if (cameraControl === "first_person") return <FPSControls />;
+
+  exhaustiveCheck(cameraControl);
 }
